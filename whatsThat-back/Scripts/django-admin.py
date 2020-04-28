d2ecac9755aeda3c10641923198beb85185b7a33
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-04-28 23:00:23 +0530

    base set and configured

---
+++ whatsThat-back/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\narut\whatsThat\whatsThat-back\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ whatsThat-back/src/images/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    picture = models.ImageField()
+    classification = models.CharField(max_length=200, blank=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Image classified at {}".format(self.uploaded.strftime("%m-%d-%Y, %H:%M"))
